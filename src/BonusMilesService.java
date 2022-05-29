ec58986a7cd64b16434cd72f44157ed538572d6d
tsvetk0va <kknn2821@mail.ru>
2022-05-29 13:48:29 +0300

    Code reformat

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
- public int calculate(int cost) {
-  int result = cost / 20;
-  return result;
- }
+    public int calculate(int cost) {
+        int result = cost / 20;
+        return result;
+    }
 
 }
